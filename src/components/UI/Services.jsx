cef1618261cb7e154b25e1883664d5a40479243f
Aditya <aditya.pandey.241103@gmail.com>
2023-07-15 23:14:24 +0530

    Portfolio Section

--- src/components/UI/Services.jsx
+++ src/components/UI/Services.jsx
@@ -23,7 +23,7 @@ const Services = () => {
                                     <div className='flex justify-start w-full mx-auto items-center'>
                                         <div className='w-full sm:w-1/2 sm:pr-8'>
                                             <div data-aos="fade-right" data-aos-duration="1200"
-                                                className='bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150'>
+                                                className='bg-white p-4 shadow-xl rounded-lg group hover:bg-primaryColor cursor-pointer ease-in duration-150'>
                                                 <h3 className='text-primaryColor font-[700] mb-3 group-hover:text-white
                                                 group-hover:font-[600] text-xl'>Frontend Development</h3>
                                                 <p className='text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500]'>
@@ -47,7 +47,7 @@ const Services = () => {
                                     <div className='flex justify-end w-full mx-auto items-center'>
                                         <div className='w-full sm:w-1/2 sm:pl-8'>
                                             <div data-aos="fade-left" data-aos-duration="1200"
-                                                className='bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150'>
+                                                className='bg-white p-4 shadow-xl rounded-lg group hover:bg-primaryColor cursor-pointer ease-in duration-150'>
                                                 <h3 className='text-primaryColor font-[700] mb-3 group-hover:text-white
                                                 group-hover:font-[600] text-xl'>Backend Development</h3>
                                                 <p className='text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500]'>
@@ -71,7 +71,7 @@ const Services = () => {
                                     <div className='flex justify-start w-full mx-auto items-center'>
                                         <div className='w-full sm:w-1/2 sm:pr-8'>
                                             <div data-aos="fade-right" data-aos-duration="1200"
-                                                className='bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150'>
+                                                className='bg-white p-4 rounded-lg shadow-xl group hover:bg-primaryColor cursor-pointer ease-in duration-150'>
                                                 <h3 className='text-primaryColor font-[700] mb-3 group-hover:text-white
                                                 group-hover:font-[600] text-xl'>Version Control </h3>
                                                 <p className='text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500]'>
@@ -95,7 +95,7 @@ const Services = () => {
                                     <div className='flex justify-end w-full mx-auto items-center'>
                                         <div className='w-full sm:w-1/2 sm:pl-8'>
                                             <div data-aos="fade-left" data-aos-duration="1200"
-                                                className='bg-white p-4 rounded shadow group hover:bg-primaryColor cursor-pointer ease-in duration-150'>
+                                                className='bg-white p-4 shadow-xl rounded-lg group hover:bg-primaryColor cursor-pointer ease-in duration-150'>
                                                 <h3 className='text-primaryColor font-[700] mb-3 group-hover:text-white
                                                 group-hover:font-[600] text-xl'>API Connection</h3>
                                                 <p className='text-[15px] text-smallTextColor group-hover:text-white group-hover:font-[500]'>
@@ -106,7 +106,7 @@ const Services = () => {
                                     </div>
                                     <div className="rounded-full bg-primaryColor border-white border-4 w-10 h-10 absolute left-1/2 transform -translate-x-1/2 -translate-y-4 sm:translate-y-0 flex items-center justify-center ">
                                         <figure>
-                                            <img src={frontendImg} alt="" />
+                                            <img src={backendImg} alt="" />
                                         </figure>
                                     </div>
 
